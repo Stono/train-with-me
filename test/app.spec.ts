2293a20f9a0f3d90541d47c89312b70f64185c50
Karl Stoney <kstoney@thoughtworks.com>
2016-08-11 21:55:16 -0300

    Half working tests, whole bunch of stack traces

---
+++ test/app.spec.ts
@@ -0,0 +1,49 @@
+import {
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS, TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+}                               from '@angular/platform-browser-dynamic/testing';
+import { setBaseTestProviders } from '@angular/core/testing';
+import { ConferenceApp } from '../app/app.ts';
+
+// this needs doing _once_ for the entire test suite, hence it's here
+setBaseTestProviders(TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+
+// Mock out Ionic's platform class
+class MockPlatform {
+  public ready(): any {
+    return new Promise((resolve: Function) => {
+      resolve();
+    });
+  }
+}
+class MockUserData {
+  public hasLoggedIn(): any {
+    return new Promise((resolve: Function) => {
+      resolve(false);
+    });
+  }
+}
+class MockMenu {}
+class MockEvents {
+  public subscribe(): any {
+    return new Promise(resolve => {
+      resolve();
+    });
+  }
+}
+
+let myApp = null;
+
+describe('App', () => {
+  beforeEach(function() {
+    let events = (<any>new MockEvents());
+    let userdata = (<any>new MockUserData());
+    let menu = (<any>new MockMenu());
+    let platform = (<any>new MockPlatform());
+
+    myApp = new ConferenceApp(events, userdata, menu, platform);
+  });
+
+  it('initialises the app', () => {
+    expect(myApp).not.toBeNull();
+  });
+});
